a64fe331558c06c3b114dbbb211a04bcc41b6521
Meng Mao <mmao@visiblemeasures.com>
2013-05-23 22:09:52 -0400

    fix ajax

--- web.rb
+++ web.rb
@@ -1,9 +1,21 @@
 require 'sinatra'
 
+# storm: ssh -i ~/szkey.pem storm@23.21.134.34
+
 get '/game/:passedgameid' do
    erb :game, :locals => { :gameid => "#{params[:passedgameid]}" }
 end
 
-get '/test1' do
-  "test 1 endpoint"
+set :left_text, ""
+set :right_text, ""
+
+get '/state' do
+
+  if params[:left]
+    set :left_text, params[:left] 
+  end
+  if params[:right]
+    set :right_text, params[:right]
+  end
+  "left: <div id='leftcontent'>" + settings.left_text + "</div><br/>right: <div id='rightcontent'>" + settings.right_text + "</div>"
 end
